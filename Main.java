c85fba660264d66fe4f8ede0649e6ea3c3564994
mehrdadarefi <me.arefi1319@gmail.com>
2025-03-26 00:07:06 +0330

    adding hashmap to my code

--- Main.java
+++ Main.java
@@ -1,38 +1,43 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What two numbers do you want to play with in multiples? ");
-
+        System.out.print("what two numbers do you want to play with in multiples? ");
         int number1 = scanner.nextInt();
         int number2 = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("what words would you like to use as 'bizz' and 'buzz'. ");
-
+        System.out.print("what words would you like to use as 'bizz' and 'buzz'. ");
         String word1 = scanner.nextLine();
         String word2 = scanner.nextLine();
 
-        System.out.println("choose your range of numbers. ");
-
+        System.out.println("choose your range of numbers: ");
         System.out.print("from ");
         int starter = scanner.nextInt();
         System.out.print(" to ");
         int endPoint = scanner.nextInt();
-        System.out.println(" is your range");
+        scanner.nextLine();
+
+        HashMap<Integer, String> game = new HashMap<>();
+        game.put(number1, word1);
+        game.put(number2, word2);
 
         for (int i = starter; i <= endPoint; i++) {
             var output = "";
 
-            if (i % number1 == 0) output += word1;
-            if (i % number2 == 0) output += word2;
-            if (output.isEmpty()) output += i;
+            for (int key : game.keySet()) {
+                if (i % key == 0) {
+                    output += game.get(key);
+                }
+            }
+            if (output.isEmpty()) {
+                output = String.valueOf(i);
+            }
 
             System.out.println(output);
-
         }
     }
 }
